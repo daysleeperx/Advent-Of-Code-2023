c438da25678689dccda67dca23b4c9388d887f33
Viktor Pavlov <viktor.pavlov@bolt.eu>
2023-11-29 00:26:38 +0200

    try to install linters and formatters

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,17 @@
 module Main (main) where
 
+data Tree a = Leaf a | Node (Tree a) a (Tree a) deriving (Show)
+
+instance Functor Tree where
+  fmap f (Leaf a) = Leaf (f a)
+  fmap f (Node left root right) = Node (fmap f left) (f root) (fmap f right)
+
+t1 :: Tree Int
+t1 = Node (Leaf 1) 2 (Leaf 3)
+
+t2 :: Tree Int
+t2 = fmap (+ 1) t1
+
 main :: IO ()
 main = do
-  putStrLn "Hello World"
+  putStrLn . show $ t2
