8ff0d59eaf0e4bc9b3ff2e8d1c1295ae0227f72b
Viktor Pavlov <viktor.pavlov@bolt.eu>
2023-11-29 23:46:52 +0200

    setup main file

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,9 @@
 module Main (main) where
 
-data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
-
-instance Functor Tree where
-  fmap _ EmptyTree = EmptyTree
-  fmap f (Node root left right) = Node (f root) (fmap f left) (fmap f right)
-
-t1 :: Tree Int
-t1 = Node 1 (Node 2 EmptyTree EmptyTree) (Node 3 EmptyTree EmptyTree)
-
-t2 :: Tree Int
-t2 = fmap (+ 1) t1
+import System.Environment
 
 main :: IO ()
 main = do
-  print t2
+  args <- getArgs
+  putStrLn "The arguments are:"
+  mapM_ putStrLn args
