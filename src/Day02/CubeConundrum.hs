a22c9cf5d50ceebc39997ae66c1171168f075a3f
Viktor Pavlov <viktor.pavlov@bolt.eu>
2023-12-03 12:42:02 +0200

    day 02 part 1

--- src/Day02/CubeConundrum.hs
+++ src/Day02/CubeConundrum.hs
@@ -11,7 +11,10 @@ import Text.Megaparsec.Char.Lexer hiding (space)
 
 type Parser = Parsec Void String
 
-newtype Game = Game (Int, Int, Int) deriving (Show)
+newtype Game = Game (Int, Int, Int) deriving (Show, Eq)
+
+instance Ord Game where
+  (Game (r, g, b)) <= (Game (r', g', b')) = r <= r' && g <= g' && b <= b'
 
 instance Semigroup Game where
   Game (r, g, b) <> Game (r', g', b') = Game (r + r', g + g', b + b')
@@ -20,33 +23,41 @@ instance Monoid Game where
   mempty = Game (0, 0, 0)
 
 parseGame :: Parser Game
-parseGame =
-  choice
-    [ Game . (,0,0) <$> try $ decimal <* string "red",
-      Game . (0,,0) <$> try $ decimal <* string "green",
-      Game . (0,0,) <$> try $ decimal <* string "blue"
-    ]
+parseGame = try red <|> try green <|> blue
+  where
+    red = Game . (,0,0) <$> decimal <* string " red"
+    green = Game . (0,,0) <$> decimal <* string " green"
+    blue = Game . (0,0,) <$> decimal <* string " blue"
 
 parseGame' :: Parser Game
-parseGame' = foldl1 (<>) <$> parseGame `sepBy1` string ", "
+parseGame' = mconcat <$> sepBy1 parseGame (string ", ")
 
-newtype GameRecord = GameRecord (Int, Game) deriving (Show)
+newtype GameRecord = GameRecord (Int, [Game]) deriving (Show)
+
+getGameId :: GameRecord -> Int
+getGameId (GameRecord (idx, _)) = idx
+
+getGames :: GameRecord -> [Game]
+getGames (GameRecord (_, games)) = games
 
 parseGameId :: Parser Int
 parseGameId = string "Game " *> decimal <* string ": "
 
 parseGameRecord :: Parser GameRecord
-parseGameRecord = do
-  gameId <- parseGameId
-  games <- parseGame' `sepBy1` string "; "
-  pure $ GameRecord (gameId, foldl1 (<>) games)
+parseGameRecord = GameRecord <$> ((,) <$> parseGameId <*> sepBy1 parseGame' (string "; "))
 
 parseGameRecords :: Parser [GameRecord]
 parseGameRecords = parseGameRecord `sepBy1` newline
+
+filterPossibleGames :: Game -> [GameRecord] -> [GameRecord]
+filterPossibleGames g = filter (all (<= g) . getGames)
+
+sumPossibleGamesIds :: Game -> [GameRecord] -> Int
+sumPossibleGamesIds g = sum . fmap getGameId . filterPossibleGames g
 
 solve :: FilePath -> IO ()
 solve filePath = do
   contents <- readFile filePath
   case parse parseGameRecords filePath contents of
     Left eb -> putStr (errorBundlePretty eb)
-    Right records -> print records
+    Right records -> print $ sumPossibleGamesIds (Game (12, 13, 14)) records
